f5a29b89181165e731a9ff879ba7b0cc6ebd81d3
cythe <harrylighter@hotmail.com>
2024-05-17 14:57:45 +0800

    point: 添加额外的point类型
    POWER SHIELD MISSILE BOMB 1UP
    
    Signed-off-by: cythe <harrylighter@hotmail.com>

--- src/point.c
+++ src/point.c
@@ -6,37 +6,82 @@
 #include "theme.h"
 #include "sound.h"
 #include "tools.h"
+#include "point.h"
 
 LIST_HEAD(points);
-SDL_Texture* pointsTexture;
+SDL_Texture* pointsTexture[POINT_TYPE_MAX];
+
+static void __gen_point_pod(int x, int y, int t)
+{
+    Point* p;
+
+    p = malloc(sizeof(Point));
+    memset(p, 0, sizeof(Point));
+    list_add(&p->list, &points);
+
+    p->r.x = x;
+    p->r.y = y;
+    p->dx = -(rand() % 5);
+    p->dy = (rand() % 5) - (rand() % 5);
+    p->health = 60 * 10;
+    p->texture = pointsTexture[t];
+
+    SDL_QueryTexture(p->texture, NULL, NULL, &p->r.w, &p->r.h);
+
+    p->r.x -= p->r.w / 2;
+    p->r.y -= p->r.h / 2;
+}
 
 void gen_point_pod(int x, int y)
 {
-    Point* e;
+    int r;
+    int t;
 
-    e = malloc(sizeof(Point));
-    memset(e, 0, sizeof(Point));
-    list_add(&e->list, &points);
+    r = rand() % 100;
+    if (r < 80) {   // 80%的概率生成奖励点
+	if ( r / 10 == 1 || r / 10 == 2 )
+	    t = POINT_TYPE_P;
+	else if ( r / 10 == 3 || r / 10 == 4 )
+	    t = POINT_TYPE_M;
+	else if ( r / 10 == 5 )
+	    t = POINT_TYPE_S;
+	else if ( r / 10 == 6 )
+	    t = POINT_TYPE_B;
+	else if ( r / 10 == 7 )
+	    t = POINT_TYPE_U;
+	else
+	    t = POINT_TYPE_P;
 
-    e->r.x = x;
-    e->r.y = y;
-    e->dx = -(rand() % 5);
-    e->dy = (rand() % 5) - (rand() % 5);
-    e->health = 60 * 10;
-    e->texture = pointsTexture;
-
-    SDL_QueryTexture(e->texture, NULL, NULL, &e->r.w, &e->r.h);
-
-    e->r.x -= e->r.w / 2;
-    e->r.y -= e->r.h / 2;
+	__gen_point_pod(x, y, t);
+    }
 }
 
 void init_point_texture(void)
 {
-    pointsTexture = loadTexture("resource/points.png");
-    if (!pointsTexture)
+    pointsTexture[POINT_TYPE_P] = loadTexture("resource/point_p.png");
+    if (!pointsTexture[POINT_TYPE_P])
 	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
-		"Couldn't load point texture: %s\n", SDL_GetError());
+		"Couldn't load point power texture: %s\n", SDL_GetError());
+
+    pointsTexture[POINT_TYPE_M] = loadTexture("resource/point_m.png");
+    if (!pointsTexture[POINT_TYPE_M])
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+		"Couldn't load point missile texture: %s\n", SDL_GetError());
+
+    pointsTexture[POINT_TYPE_S] = loadTexture("resource/point_s.png");
+    if (!pointsTexture[POINT_TYPE_S])
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+		"Couldn't load point shield texture: %s\n", SDL_GetError());
+
+    pointsTexture[POINT_TYPE_B] = loadTexture("resource/point_b.png");
+    if (!pointsTexture[POINT_TYPE_B])
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+		"Couldn't load point bomb texture: %s\n", SDL_GetError());
+
+    pointsTexture[POINT_TYPE_U] = loadTexture("resource/point_u.png");
+    if (!pointsTexture[POINT_TYPE_U])
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+		"Couldn't load point 1up texture: %s\n", SDL_GetError());
 }
 
 void logic_points(void)
@@ -75,7 +120,9 @@ void logic_points(void)
 	if (player != NULL && collision(&e->r, &player->r)) {
 	    e->health = 0;
 
-	    g_player.bullet_level++;
+	    if(g_player.bullet_level < PLAYER_BULLET_LEVEL_MAX) {
+		g_player.bullet_level++;
+	    }
 
 	    highscore = MAX(score, highscore);
 
--- src/point.h
+++ src/point.h
@@ -1,6 +1,15 @@
 #pragma once
 #include "type.h"
 
+enum {
+    POINT_TYPE_P = 0,   // Power
+    POINT_TYPE_M = 1,   // Missile
+    POINT_TYPE_S = 2,   // Shield
+    POINT_TYPE_B = 3,   // Bomb
+    POINT_TYPE_U = 4,   // 1 Up
+    POINT_TYPE_MAX,
+};
+
 extern struct list_head points;
 
 void init_point_texture(void);
